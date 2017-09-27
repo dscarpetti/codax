8f51dfc4e7cffd64ee811bf731fabcfa6a7f34d7
David Scarpetti <dscarpetti@gmail.com>
2017-09-27 14:49:07 -0600

    doc strings

--- src/codax/swaps.clj
+++ src/codax/swaps.clj
@@ -1,11 +1,17 @@
 (ns codax.swaps)
 
-(defn inc-count [x]
+(defn inc-count
+  "Used for incrementing a counter that may not be initialized.
+  If `x` is not a positive integer, `inc-count` evaluates to 1"
+  [x]
   (if (and (integer? x) (pos? x))
     (inc x)
     1))
 
-(defn dec-count [x]
+(defn dec-count
+  "Used for decrementing a counter that may not be initialized.
+  If `x` is not a positive integer, `dec-count` evaluates to 0"
+  [x]
   (if (and (integer? x) (> x 1))
     (dec x)
     0))
