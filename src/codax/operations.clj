77836ff0e8050bbdfa81e0b584261f02a6fdc3ba
David Scarpetti <dscarpetti@gmail.com>
2017-11-20 15:48:22 -0700

    simple and chunking seek-path implementations
    api still in flux

--- src/codax/operations.clj
+++ src/codax/operations.clj
@@ -82,6 +82,117 @@
   [tx path]
   (get-in (reduce reduce-assoc {} (seek tx path)) path))
 
+
+
+;;;;;;;;;;;;
+
+(defn- assemble-chunk [lead-trim data complete active-key active-data limit last-key]
+  (if (or (= limit 0) (empty? data))
+    [complete active-key active-data limit last-key]
+    (let [[raw-k v] (first data)
+          [k & ks] (drop lead-trim (pathwise/decode raw-k))]
+      (if (or (= active-key k) (= active-key ::none))
+        (recur lead-trim
+               (rest data)
+               complete
+               k
+               (if (empty? ks) v (assoc-in active-data ks v))
+               limit
+               raw-k)
+        (recur lead-trim
+               (rest data)
+               (conj! complete [active-key active-data])
+               k
+               (if (empty? ks) v (assoc-in {} ks v))
+               (dec limit)
+               raw-k)))))
+
+(defn- seek-path-chunk
+  [tx lead-trim start-path end-path limit partial]
+  (let [chunk-size (if (and (number? limit) (pos? limit))
+                     (max (int (* 1.5 limit)) 10)
+                     nil)
+        limit (if (and (number? limit)) limit -1)]
+    (persistent!
+     (loop [chunk (seek tx start-path end-path :limit chunk-size :partial partial :no-decode true)
+            results (transient [])
+            active-key ::none
+            active-data nil
+            limit limit]
+       (if (or (= limit 0) (empty? chunk))
+         (if (= active-key ::none)
+           results
+           (conj! results [active-key active-data]))
+         (let [prev-limit limit
+               [complete active-key active-data limit last-key]
+               (assemble-chunk lead-trim chunk results active-key active-data limit nil)]
+           (if (= limit 0)
+             complete
+             (recur
+              (rest (seek tx (pathwise/decode last-key) end-path :limit chunk-size :partial partial :no-decode true))
+              complete
+              active-key
+              active-data
+              limit))))))))
+
+
+(defn seek-path
+  ([tx path limit]
+   (seek-path-chunk tx (count path) path path limit false))
+
+  ([tx path prefix limit]
+   (let [seek-path (conj path prefix)]
+     (seek-path-chunk tx (count path) seek-path seek-path limit true)))
+
+  ([tx path start-val end-val limit]
+   (if (or (nil? end-val) (pos? (compare start-val end-val)))
+     []
+     (let [start-path (conj path start-val)
+           end-path (if (nil? end-val) nil (conj path end-val))]
+       (seek-path-chunk tx (count path) start-path end-path limit false)))))
+
+;;;;;;;
+
+(defn- assemble-seek-simple [lead-trim data]
+  (loop [raw data
+         complete []
+         active-key ::none
+         active-data nil]
+    (if (empty? raw)
+      (if (= active-key ::none)
+        complete
+        (conj complete [active-key active-data]))
+      (let [[raw-k v] (first raw)
+            [k & ks] (drop lead-trim (pathwise/decode raw-k))]
+        (if (or (= active-key k) (= active-key ::none))
+          (recur (rest raw)
+                 complete
+                 k
+                 (if (empty? ks) v (assoc-in active-data ks v)))
+          (recur (rest raw)
+                 (conj complete [active-key active-data])
+                 k
+                 (if (empty? ks) v (assoc-in {} ks v))))))))
+
+
+(defn seek-path-simple
+  ([tx path]
+   (assemble-seek-simple (count path)
+                       (seek tx path path :no-decode true)))
+  ([tx path prefix]
+   (let [seek-path (conj path prefix)]
+     (assemble-seek-simple (count path)
+                         (seek tx seek-path seek-path :no-decode true :partial true))))
+
+  ([tx path start-val end-val]
+   (let [start-path (conj path start-val)
+         end-path (conj path end-val)]
+     (assemble-seek-simple (count path)
+                         (seek tx start-path end-path :no-decode true)))))
+
+
+;;;;;
+
 (defn- validate-path [tx path]
   (when (empty? path) (throw (ex-info "Invalid Path" {:cause :empty-path
                                                       :message "You cannot modify the empty (root) path."})))
