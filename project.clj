f609c029151bdc2d8730eb1ffc063615d72845ad
David Scarpetti <dscarpetti@gmail.com>
2017-11-25 20:03:05 -0700

    move dev utils and deps to :dev lein profile
    renamed the dev utils to tree_info

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.taoensso/nippy "2.12.2"]
                  [clj-time "0.13.0"]
-                 [org.clojars.kliph/vijual "0.2.7"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.cache "0.6.5"]]
   :main codax.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[org.clojars.kliph/vijual "0.2.7"]]
+                   :source-paths ["dev"]}})
