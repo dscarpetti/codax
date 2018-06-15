5b8c49ced920d0b654972edb5b21c1bb297cfe5d
David Scarpetti <dscarpetti@gmail.com>
2018-06-15 15:43:12 -0600

    throw `ExceptionInfo` when attempting to encode unrecognized type
    This required rewriting the Boolean and Number handling so that the
    protocol dispatching is not done on `java.lang.Object`

--- src/codax/pathwise.clj
+++ src/codax/pathwise.clj
@@ -14,11 +14,20 @@
   [unrecognized-el]
   (let [hex-code (str "0x" (int (char (first unrecognized-el))))
         s (subs unrecognized-el 1 (dec (count unrecognized-el)))]
-    (throw (ex-info "Path Decoding Error: Unrecognized Encoding"
-                    {:cause :unrecognized-encoding
+    (throw (ex-info "Path Element Decoding Error: No Matching Decoder"
+                    {:cause :no-matching-decoder
                      :message "No matching decoder was found for the given encoded element's prefix"
                      :hex-code hex-code
                      :element-as-string s}))))
+
+(extend-type java.lang.Object
+  PathwiseEncoding
+  (encode [x]
+    (throw (ex-info (str "Path Element Encoding Error: No Matching Encoder For: " (type x))
+                    {:cause :no-matching-encoder
+                     :message "No matching encoder was found for the given element's type"
+                     :type (type x)
+                     :element x}))))
 
 (defn partially-encode [x]
   (str/replace (encode x) (re-pattern (str +delim+ "+$")) ""))
@@ -78,79 +87,52 @@
                       :else (recur ts (conj result t))))]
        (mapv decode result))))
 
-(defn te [form]
-  (let [encoded (encode form)
-        decoded (decode encoded)]
-    (println form)
-    (println decoded)
-    (= form decoded)))
+;;;; Support for encodings of numbers and booleans
+;; due to previous design choices which cannot be changed due to backward-compatibility issues
+;; numbers and booleans cannot be defined using `defpathtype`
 
-;;(te [[[[[[]]]:a]]])
+(defn- manual-encode
+  ([hex-code] (str (char hex-code) +delim+))
+  ([hex-code body] (str (char hex-code) body +delim+)))
 
-;; support for legacy encodings of numbers and booleans
+;; boolean encoding/decoding
+
+(extend-type java.lang.Boolean
+  PathwiseEncoding
+  (encode [x] (manual-encode (if x 0x21 0x20))))
+
+(defmethod decode (char 0x20) [_] false)
+(defmethod decode (char 0x21) [_] true)
+
+;; number encoding/decoding
+
 (defn- encode-number [n]
-  (let [n (if (ratio? n) (double n) n)
-        offset (if (<= 0 n) 0 -1000)
+  (let [offset (if (<= 0 n) 0 -1000)
         prefix (if (<= 0 n) "_" "-")
         n-str (if (integer? n) (str n) (format "%f" n))
         n-str (if (<= 0 n) n-str (reduce str (map #(if (= \. %) "." (str (- 9 (read-string (str %))))) (subs n-str 1))))
         len (count (re-find #"[^.]*" n-str))]
     (str prefix (re-find #"\d\d\d$" (str "0" "0" (+ offset len))) "x" n-str)))
 
-(defn decode-number [n]
+(defn- decode-number [n]
   (read-string
    (if (= \- (first n))
      (reduce str "-" (map (fn [x] (if (= \. x) x (- 9 (read-string (str x)))) ) (nthrest n 5)))
      (reduce str "" (nthrest n 5)))))
 
-(defn- manual-encode
-  ([hex-code] (str (char hex-code) +delim+))
-  ([hex-code body] (str (char hex-code) body +delim+)))
+(defmacro extend-types [ts & specs]
+  `(do
+     ~@(map (fn [t] `(extend-type ~t ~@specs)) ts)))
 
-(extend-type java.lang.Object
-  PathwiseEncoding
-  (encode [x]
-    (cond
-      (false? x) (manual-encode 0x20)
-      (true? x) (manual-encode 0x21)
-      (and (number? x) (Double/isInfinite x) (neg? x)) (manual-encode 0x30)
-      (and (number? x) (Double/isInfinite x) (pos? x)) (manual-encode 0x32)
-      (number? x) (manual-encode 0x31 (encode-number x)))))
+(extend-types
+ [java.lang.Byte java.lang.Short java.lang.Integer java.lang.Long java.lang.Float java.lang.Double]
+ PathwiseEncoding
+ (encode [x]
+         (cond
+           (and (Double/isInfinite x) (neg? x)) (manual-encode 0x30)
+           (and (Double/isInfinite x) (pos? x)) (manual-encode 0x32)
+           :else (manual-encode 0x31 (encode-number x)))))
 
-(defmethod decode (char 0x20) [_] false)
-(defmethod decode (char 0x21) [_] true)
 (defmethod decode (char 0x30) [_] Double/NEGATIVE_INFINITY)
 (defmethod decode (char 0x31) [el] (decode-number (subs el 1 (dec (count el)))))
 (defmethod decode (char 0x32) [_] Double/POSITIVE_INFINITY)
-
-(comment
-  "This is a possible future upgrade path which would break backward-compatibility for databases using:"
-  "`true` `false` `NEGATIVE_INFINITY` or `POSITIVE_INFINITY` in paths."
-
-  (defpathtype [0x20 java.lang.Boolean]
-    #(if % "t" "f")
-    #(= % "t"))
-
-  (defpathtype [0x31 java.lang.Byte java.lang.Short java.lang.Integer java.lang.Long java.lang.Float java.lang.Double]
-    #(cond
-       (and (Double/isInfinite %) (neg? %)) "!"
-       (Double/isInfinite %) "i"
-       :else (let [offset (if (<= 0 %) 0 -1000)
-                   prefix (if (<= 0 %) "_" "-")
-                   n-str (if (integer? %) (str %) (format "%f" %))
-                   n-str (if (<= 0 %) n-str (reduce str (map (fn [x] (if (= \. x) "." (str (- 9 (read-string (str x)))))) (subs n-str 1))))
-                   len (count (re-find #"[^.]*" n-str))]
-               (str prefix (re-find #"\d\d\d$" (str "0" "0" (+ offset len))) "x" n-str)))
-    #(cond
-       (= % "!") Double/NEGATIVE_INFINITY
-       (= % "i") Double/POSITIVE_INFINITY
-       :else (read-string
-              (if (= \- (first %))
-                (reduce str "-" (map (fn [x] (if (= \. x) x (- 9 (read-string (str x)))) ) (nthrest % 5)))
-                (reduce str "" (nthrest % 5))))))
-
-;;; compatibility decoders for old encodings
-  (defmethod decode (char 0x21) [_] true)
-  (defmethod decode (char 0x30) [_] Double/NEGATIVE_INFINITY)
-  (defmethod decode (char 0x32) [_] Double/POSITIVE_INFINITY)
-  )
